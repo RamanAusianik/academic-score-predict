c8bdfb021416e46a91660e2c5ef50b08d6ee5c42
Andrei Ausianik <andrey.ausianik@gmail.com>
2026-05-30 19:36:28 +0200

    electron init

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // Allows us to use Node.js features in the frontend if needed
+      nodeIntegration: true, 
+      contextIsolation: false
+    }
+  })
+
+  // Load the index.html of the app.
+  mainWindow.loadFile('./build/index.html')
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
